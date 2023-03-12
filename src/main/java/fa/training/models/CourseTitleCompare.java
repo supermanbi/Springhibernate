3123ec9c676e7de03f6f864dbfca7b439ea8063f
Khanh <duykhanh21703@gmail.com>
2023-03-12 09:46:29 +0700

    first commit

---
+++ src/main/java/fa/training/models/CourseTitleCompare.java
@@ -0,0 +1,11 @@
+package fa.training.models;
+
+import java.util.Comparator;
+
+public class CourseTitleCompare implements Comparator<Course> {
+	public int compare(Course o1, Course o2) {
+		return o1.getId().compareToIgnoreCase(o2.getId());
+	}
+
+}
+
---
+++ src/main/java/fa/training/models/Statistical.java
@@ -0,0 +1,10 @@
+package fa.training.models;
+
+import java.util.Map;
+
+public interface Statistical {
+	
+	public Map<String, Integer> getStatistic();
+	
+}
+
